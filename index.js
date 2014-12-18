9e779e01093f7498118db2ee01e15f9b2e46a9c6
Michaël Zasso <mic.besace@gmail.com>
2014-12-18 11:18:07 +0100

    nodeify async methods (now working as expected)

--- index.js
+++ index.js
@@ -2,6 +2,17 @@
 var promisify = require('promisify-lib'),
     find = require('find');
 
+function nodeify(fn) {
+    return function (pattern, root, callback) {
+        fn.call(null, pattern, root, function (result) {
+            callback(null, result);
+        }).error(callback);
+    }
+}
+
+find.file = nodeify(find.file);
+find.dir = nodeify(find.dir);
+
 promisify.all(find, exports, [
     'file',
     'dir'
